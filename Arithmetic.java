17343d708c2833a53cee27c89343c13917204608
zubiri <zubiri@Ubuntu-12-04.(none)>
2014-09-23 08:38:20 +0200

    ariketak

---
+++ Arithmetic.java
@@ -0,0 +1,48 @@
+/*
+ * Clase para obtener resultados de distintas operaciones aritméticas.
+ */
+public class Arithmetic {
+
+
+ //Método que suma dos números enteros y devuelve el resultado.
+ public int sum (int num1, int num2) {
+
+	int result = 0;
+	result = num1 + num2;
+	return result;
+ }
+
+
+
+ //Método que resta dos números enteros y devuelve el resultado.
+ public int rest (int num1, int num2) {
+
+	int result = 0;
+	result = num1 - num2;
+	return result;
+ }
+
+//Método que divide dos números enteros y devuelve el resultado.
+ public int div (int num1, int num2) {
+
+	int result = 0;
+	result = num1 / num2;
+	return result;
+ }
+
+//Método que multiplica dos números enteros y devuelve el resultado.
+ public int multiply (int num1, int num2) {
+
+	int result = 0;
+	result = num1 * num2;
+	return result;
+ }
+
+//Método que divide dos números enteros y devuelve el resto.
+ public int mod (int num1, int num2) {
+
+	int result = 0;
+	result = num1 * num2;
+	return result;
+ }
+}
